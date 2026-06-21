86ffd43b41b5fbd7d8eaf302e2cb1dbe84dc8b76
AlthArsy <althafgitrif23@gmail.com>
2026-06-21 13:55:37 +0700

    awal Juni 21 2026

--- koneksi.php
+++ koneksi.php
@@ -1,5 +1,5 @@
 <?php
-$koneksi = mysqli_connect("localhost", "root", "Admin123", "Tester");
+$koneksi = mysqli_connect("localhost", "Zee", "Admin1234!", "tester");
 if (!$koneksi) {
     die("Koneksi gagal: " . mysqli_connect_error());
 }
